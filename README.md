38edd48dbdb4272eb02dc751c377617ab370f2e6
hvpexe <phuhuynh923@gmail.com>
2022-09-21 16:44:36 +0700

    refactor the main code and green 4ever always

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 ### You will find in this repo the following stuff:
 * The Math-Util library that offers multiple math functions (a Java-based source code)
 * Unit Testing source code with JUnit framework
+* Github Actions to demonstrate the Continuous Integration (CI) technique
+* Regression Test
+* ...
 
 #### © 2022 hvpexe
--- src/com/hvpexe/mathutil/core/MathUtil.java
+++ src/com/hvpexe/mathutil/core/MathUtil.java
@@ -22,17 +22,26 @@ public class MathUtil {
     //20 giai thừa 18 số 0, vừa kịp đủ cho kiểu long của java
     //21 giai thừa tràn kiểu long
     //bài này quy ước tính n! trong khoảng từ 0..20
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) {
+        if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid argument. N must be between 0..20");
-        }
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        if (n == 0 || n == 1) 
+            return 1; //Kết thúc cuộc chơi sớm nếu nhận những đầu vào đặt biệt
+        return n * getFactorial(n-1); //công thức đệ quy
     }
+    
 }
